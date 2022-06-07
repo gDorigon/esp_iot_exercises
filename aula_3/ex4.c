27e6d4b1d7074fef1bfa69b6ed1c3378e276f568
Guilherme Dorigon <guidbovo@gmail.com>
2022-06-07 16:42:29 -0300

    Create ex4.c
    O ESP32 possui um sensor hall integrado. Descubra
    como fazer leituras dele e apresente os dados a cada 500ms,
    utilizando tasks.

---
+++ aula_3/ex4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_log.h"
+
+void magnetico(void *arg){
+
+    int mag=0;
+
+    while(true) {
+
+    mag = hall_sensor_read();
+    printf("measurement: %d\n", mag);
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+    }
+
+    vTaskDelay(NULL);
+}
+
+void app_main(){
+    int measurement = 0; 
+
+
+    while(true){
+
+    xTaskCreatePinnedToCore(&magnetico, "mag reading", 2048, NULL, 2, NULL, 1);
+    vTaskDelay(300);
+
+    }
+}
